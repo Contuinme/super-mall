476891bd7ebc37099dc1d84974717f1a6a367fa3
wyz <806543499@qq.com>
2020-10-09 23:16:24 +0800

    基本项目框架+tabbar+navbar

---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav-bar class="home-nav"><div slot="center">不拿钱就拿命购物站</div></nav-bar>
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
