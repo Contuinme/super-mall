1ac74b8cd6d0290205f5dce9004353b41e881ce6
wyz <806543499@qq.com>
2020-10-11 18:23:45 +0800

    首页数据展示

---
+++ src/views/home/ChildComps/FeatureView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a href="https://act.mojujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+name: "FeatureView"
+}
+</script>
+
+<style scoped>
+  img {
+    width: 100%;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,47 +1,108 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav"><div slot="center">不拿钱就拿命购物站</div></nav-bar>
-<<<<<<< HEAD
-    <h1>首页</h1>
-=======
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommend="recommends"></home-recommend-view>
->>>>>>> master
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodsList from "@/components/content/goods/GoodsList";
+
+import FeatureView from "@/views/home/ChildComps/FeatureView";
 import HomeSwiper from "@/views/home/ChildComps/HomeSwiper";
 import HomeRecommendView from "@/views/home/ChildComps/HomeRecommendView";
 
-import {getHomeMultidata} from "@/network/home";
+import {getHomeMultidata,getHomeGoods} from "@/network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    TabControl,
+    FeatureView,
     HomeSwiper,
-    HomeRecommendView
+    HomeRecommendView,
+    GoodsList
   },
   data(){
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goodsList: {
+        'pop': {
+          page: 0,
+          list: []
+        },
+        'new': {
+          page: 0,
+          list: []
+        },
+        'sell': {
+          page: 0,
+          list: []
+        }
+      },
+      currentType: 'pop'
     }
   },
   created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+    this.getHomeMultidata();
+
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata(){
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type){
+      getHomeGoods(type,this.goodsList[type].page + 1).then(res => {
+        this.goodsList[type].list.push(...res.data.list)
+        this.goodsList[type].page += 1
+      })
+    },
+    tabClick(index) {
+      switch (index) {
+        case 0:this.currentType = 'pop'
+          break
+        case 1:this.currentType = 'new'
+          break
+        case 2:this.currentType = 'sell'
+          break
+      }
+    }
+  },
+  computed: {
+    showGoods() {
+      return this.goodsList[this.currentType].list
+    }
   }
 }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    z-index: 9;
   }
 </style>
