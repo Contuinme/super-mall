2939f08099e6084b56c3baeb620834035cfc1430
wangnanxiang <806543499@qq.com>
2020-10-19 00:13:45 +0800

    详情页完成

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="detail">
     <detail-nav-bar id="detail-nav-bar"></detail-nav-bar>
-    <scroll class="content">
+    <scroll ref="scrollComp" class="content">
       <detail-swiper :top-images="topImages"></detail-swiper>
       <detail-base-info :goods="goods"></detail-base-info>
       <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @loadImgEvent="loadImgEvent"></detail-goods-info>
+      <detail-params :param-info="paramInfo" id="detail-params"></detail-params>
     </scroll>
   </div>
 </template>
@@ -14,26 +16,37 @@ import DetailNavBar from "@/views/detail/childComps/DetailNavBar";
 import DetailSwiper from "@/views/detail/childComps/DetailSwiper";
 import DetailBaseInfo from "@/views/detail/childComps/DetailBaseInfo";
 import DetailShopInfo from "@/views/detail/childComps/DetailShopInfo";
+import DetailGoodsInfo from "@/views/detail/childComps/DetailGoodsInfo";
+import DetailParams from "@/views/detail/childComps/DetailParams";
 
-import {getDetail,Goods,Shop} from "@/network/detail";
+import {getDetail,Goods,Shop,GoodsParam} from "@/network/detail";
 import Scroll from "@/components/common/scroll/scroll";
 
 export default {
   name: "Detail",
-  components: {DetailShopInfo, Scroll, DetailBaseInfo, DetailSwiper, DetailNavBar},
+  components: {DetailParams, DetailGoodsInfo, DetailShopInfo, Scroll, DetailBaseInfo, DetailSwiper, DetailNavBar},
   comments: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
     DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParams,
     Scroll
+  },
+  methods: {
+    loadImgEvent() {
+      this.$refs.scrollComp.refresh()
+    }
   },
   data() {
     return {
       iid: -1,
       topImages: [],
       goods: {},
-      shop: {}
+      shop: {},
+      detailInfo: {},
+      paramInfo: {}
     }
   },
   created() {
@@ -46,6 +59,10 @@ export default {
       this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
 
       this.shop = new Shop(data.shopInfo)
+
+      this.detailInfo = data.detailInfo
+
+      this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
     })
   }
 }
@@ -65,5 +82,10 @@ export default {
   }
   .content {
     height: calc(100% - 44px);
+  }
+  #detail-params {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
   }
 </style>
